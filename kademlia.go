4b213dc8932bcaacb951b39cded51520b920945a
henpet <henpet@users.noreply.github.com>
2018-09-20 12:51:32 +0000

    kademlia: Add NewKademlia function

--- kademlia.go
+++ kademlia.go
@@ -1,6 +1,17 @@
 package d7024e
 
 type Kademlia struct {
+	rt   *RoutingTable
+	netw *Network
+}
+
+func NewKademlia(me Contact, port string) *Kademlia {
+	rt := NewRoutingTable(me)
+	// hardcoded port for now
+	netw := NewNetwork(port, me.Address)
+	k := &Kademlia{rt: rt,
+		netw: netw}
+	return k
 }
 
 func (kademlia *Kademlia) LookupContact(target *Contact) {
