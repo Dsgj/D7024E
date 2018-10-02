3f9980ab29bef1ef94bb638fb7739ac034ec07ea
henpet <henrikpettersson92@gmail.com>
2018-10-02 18:35:30 +0000

    changed bootstrap to perform an iterativeFindNode instead

--- app/main.go
+++ app/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
 )
 
 const (
@@ -38,18 +39,25 @@ func main() {
 
 func bootstrap(k *d.Kademlia, me d.Contact) {
 	bs := d.NewContact(d.NewKademliaID(bootstrapID), bootstrapAddr)
-	closestContacts, err, timeout := k.FIND_NODE(bs, me.ID.String())
+	// closestContacts, err, timeout := k.FIND_NODE(bs, me.ID.String())
+	k.Update(bs)
+	closestContacts, err := k.IterativeFindNode(me.ID.String())
 	if err != nil {
 		log.Fatal(err)
 	}
-	if timeout {
-		log.Printf("Request timed out\nRestarting bootstrap")
+	// if timeout {
+	// 	log.Printf("Request timed out\nRestarting bootstrap")
+	// 	bootstrap(k, me)
+	// }
+	if len(closestContacts) == 0 {
+		log.Printf("Received no contacts\nRestarting bootstrap")
+		time.Sleep(time.Second * 5)
 		bootstrap(k, me)
 	}
 	fmt.Printf("received contacts: %+v\n", closestContacts)
-	for _, contact := range closestContacts {
-		k.Update(contact)
-	}
+	// for _, contact := range closestContacts {
+	// 	k.Update(contact)
+	// }
 }
 
 func getSelf(ip string) d.Contact {
