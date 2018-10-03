ceea97302f161612f283407b046503274a0bc07d
henpet <henrikpettersson92@gmail.com>
2018-10-03 16:38:10 +0000

    main.go: Cleanup unused functions

--- app/main.go
+++ app/main.go
@@ -4,7 +4,6 @@ import (
 	d "D7024E/dht"
 	"fmt"
 	"log"
-	"net"
 	"os"
 	"time"
 )
@@ -56,44 +55,5 @@ func getSelf(ip string) d.Contact {
 	} else {
 		id := d.NewRandomKademliaID()
 		return d.NewContact(id, ip)
-	}
-}
-
-// gets preferred outbound IP
-func GetOutboundIP() net.IP {
-	conn, err := net.Dial("udp", "8.8.8.8:80")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
-
-	return localAddr.IP
-}
-
-// prints all IPs
-func ListIPs() {
-	ifaces, err := net.Interfaces()
-	if err != nil {
-		log.Fatal(err)
-	}
-	count := 1
-	for _, i := range ifaces {
-		addrs, err := i.Addrs()
-		if err != nil {
-			log.Fatal(err)
-		}
-		for _, addr := range addrs {
-			var ip net.IP
-			switch v := addr.(type) {
-			case *net.IPNet:
-				ip = v.IP
-			case *net.IPAddr:
-				ip = v.IP
-			}
-			log.Printf("IP Address %d: %d", count, ip)
-			count++
-		}
 	}
 }
