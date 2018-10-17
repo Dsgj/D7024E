252510a0e91b98b8f7756f973a8f988e257fbda1
Leo <leoozolinscarlson@gmail.com>
2018-10-17 16:54:45 +0200

    Add files via upload

---
+++ dht/network_test.go
@@ -0,0 +1,19 @@
+package dht
+
+import (
+	"testing"
+)
+
+
+//TODO: FIND_NODE test
+func Test_FIND_NODE(t *testing.T) {
+
+}
+
+func TestNetwork_SendRequest(t *testing.T) {
+
+}
+
+func TestNetwork_SendMessage(t *testing.T) {
+
+}
