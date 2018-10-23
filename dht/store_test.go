7b127e14d136a325afb26f77bec598adb5612cfc
hanlog-4 <hanlog-4@student.ltu.se>
2018-10-23 21:42:11 +0200

    Added test for Store

--- dht/store_test.go
+++ dht/store_test.go
@@ -4,62 +4,37 @@ import (
 	"fmt"
 	"testing"
 	"time"
+	"math/rand"
 	//"encoding/hex"
 )
 
 func TestStore(t *testing.T) {
-	Teststore(t)
-}
+	
+	contacts, _, teardown := setupTestCase(t, 20)
+	
+	defer teardown(t)
 
-func Teststore(t *testing.T) {
-	stor1 := NewStore()
-	data := []byte{'X'}
-	contact := NewContact(NewKademliaID("ffffffff00000000000000000000000000000000"), "192.169.0.0")
-
-	//Tests store
-	_ = stor1.Store(data, contact, time.Now())
-
-	//Test getRecord with both a key that exists and with one that doesnt exist
-	key := GetKey(data)
-	record, _ := stor1.GetRecord(key)
-	data2 := []byte{'X', 'Y', 'Z'}
-	keyFake := GetKey(data2)
-	stor1.GetRecord(keyFake)
-
-	//Tests PinRecord() with both a real key and a false key(a key to data that is not stored)
-	stor1.PinRecord(key)
-	stor1.PinRecord(keyFake)
-
-	//Tests DelRecord. Tries to delete a record that does not exist, a record that is pinned and a record that is real and unpinned
-	stor1.DelRecord(key)
-	stor1.UnpinRecord(key)
-	stor1.DelRecord(key)
-	stor1.DelRecord(keyFake)
-
-	//Tests UnpinRecord by trying to unpin with a fake key
-	stor1.UnpinRecord(keyFake)
-
-	//Tests SendableRecord() by passing both a real and a fake key
-	stor1.Store(data, contact, time.Now())
-	stor1.SendableRecord(key, false)
-	stor1.SendableRecord(keyFake, true)
-
-	//Tests Republish()
-	record.Republish(time.Now())
-
-	//Tests Replicate()
-	record.Replicate(time.Now())
-
-	//Tests IsExpired()
-	record.IsExpired(time.Now())
-
-	//Tests NeedsReplicate()
-	record.NeedsReplicate(time.Now())
-
-	//Tests NeedRepublish()
-	record.NeedsRepublish(time.Now(), contact)
-
-	Testfromstring(t)
+	rand.Seed(time.Now().UnixNano())
+	N := rand.Intn(9)
+	N += 1
+	data := make([]byte, N)
+	for i := 0; i < N; i++ {
+		data[i] = 'a' + byte(i%26)
+	}
+	
+	store := NewStore()
+	
+	rec := store.Store(data, contacts[1], time.Now())
+	
+	for i := 0; i < len(data); i++ {
+		if data[i] != rec.value[i] {
+			t.Errorf("Store stored the wrong value. Stored: %d. Should store: %d.\n", rec.value, data)
+		}
+	}
+	if rec.publisher != contacts[1] {
+		t.Errorf("Store stored the wrong publisher. Stored %d. Should store: %d.\n", rec.publisher, contacts[1])
+	}
+	
 }
 
 func Testfromstring(t *testing.T) {
@@ -74,4 +49,3 @@ func Testfromstring(t *testing.T) {
 	FromString("48656c6c6f20476f7068657221")
 	fmt.Println(FromString("48656c6c6f20476f7068657221"))
 }
-
